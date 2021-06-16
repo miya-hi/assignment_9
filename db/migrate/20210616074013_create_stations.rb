00ab919572e9085d029af76a76eda852ae225528
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-16 16:43:03 +0900

    マイグレーションファイル作成

--- db/migrate/20210616074013_create_stations.rb
+++ db/migrate/20210616074013_create_stations.rb
@@ -1,7 +1,9 @@
 class CreateStations < ActiveRecord::Migration[5.2]
   def change
     create_table :stations do |t|
-
+      t.text :line
+      t.text :name
+      t.text :time
       t.timestamps
     end
   end
